935f1ff85c23bacd2d62b654f3ca156cc4401f2c
muzadn <muzamil.adnan@gildwaretechnologies.com>
2024-08-14 10:33:42 +0530

    all issues fixed

--- backend/db/connectToMongoDb.js
+++ backend/db/connectToMongoDb.js
@@ -3,7 +3,11 @@ export const connectToMongoDb =async()=>{
 
 try{
     console.log(process.env.MONGO_DB_URI)
-await mongoose.connect(process.env.MONGO_DB_URI);
+await mongoose.connect(process.env.MONGO_DB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 20000 // Increase to 20 seconds
+});
 console.log("connected to mongodb")
 }
 catch(err){
--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -6,26 +6,28 @@ import toast from 'react-hot-toast';
 
 const useListenMessages = () => {
   const { socket } = useSocketContext();
-  const { messages, setMessages, selectedConversation, incrementUnreadMessages } = useConversation();
+  const { setMessages, selectedConversation, incrementUnreadMessages, messages } = useConversation();
 
   useEffect(() => {
-    socket?.on('newMessage', (newMessage) => {
+    const handleNewMessage = (newMessage) => {
       const sound = new Audio(notificationSound);
       sound.play();
 
       // Check if the new message is from the currently selected conversation
       if (newMessage.senderId === selectedConversation?._id) {
-        setMessages( [...messages, newMessage]);
+        setMessages([...messages, newMessage]);
         newMessage.shouldShake = false; // Mark as read
       } else {
         toast.success('You have a new message');
         incrementUnreadMessages(newMessage.senderId); // Increment unread count for other conversations
         newMessage.shouldShake = true;  // Mark as unread
       }
-    });
+    };
 
-    return () => socket.off('newMessage');
-  }, [socket, selectedConversation, incrementUnreadMessages, setMessages]);
+    socket?.on('newMessage', handleNewMessage);
+
+    return () => socket.off('newMessage', handleNewMessage);
+  }, [socket, selectedConversation, incrementUnreadMessages, setMessages, messages]);
 };
 
 export default useListenMessages;
